3ffae4927e6da60d276a28712b0048a07df37494
lucjmac <lucasjmacias@hotmail.com>
2023-04-08 17:07:39 -0300

    se agrega la validación de campos al formulario.

--- DESARROLLADOR-WEB-INICIAL---TPI---Clase-164---Grupo-04/script.js
+++ DESARROLLADOR-WEB-INICIAL---TPI---Clase-164---Grupo-04/script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-const form = document.querySelector('#contacto');
+  const form = document.querySelector('#contacto');
   form.addEventListener('submit', (event) => {
     event.preventDefault();
     console.log('Formulario enviado');
@@ -11,9 +11,26 @@ const form = document.querySelector('#contacto');
     console.log(`Teléfono: ${telefono}`);
     const mensaje = document.querySelector('#contacto textarea[name="mensaje"]').value;
     console.log(`Mensaje: ${mensaje}`);
+
+    const regexTelefono = /^[0-9]{10}$/;
+    if (!regexTelefono.test(telefono)) {
+      alert('Por favor, ingrese un número de teléfono válido (10 dígitos).');
+      return;
+    }
+
+    const regexCorreo = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!regexCorreo.test(correo)) {
+      alert('Por favor, ingrese una dirección de correo electrónico válida.');
+      return;
+    }
+
     const pdf = new jsPDF();
     pdf.text(`Nombre: ${nombre}\nCorreo: ${correo}\nTeléfono: ${telefono}\nMensaje: ${mensaje}`, 10, 10);
     pdf.save("contacto.pdf");
+
+    form.reset();
   });
 
 });
+
+
