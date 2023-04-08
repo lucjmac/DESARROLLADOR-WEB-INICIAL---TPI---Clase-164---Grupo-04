292d1d459392366f03561e2c295e63f5c8b8db6d
unknown <lucjmac>
2023-04-08 14:58:58 -0300

    se agregó una funcion que exporta a pdf y se ajustaron cuestiones esteticas de las secciones.

--- DESARROLLADOR-WEB-INICIAL---TPI---Clase-164---Grupo-04/script.js
+++ DESARROLLADOR-WEB-INICIAL---TPI---Clase-164---Grupo-04/script.js
@@ -1,15 +1,19 @@
-var inputs = document.getElementsByClassName('formulario__input');
-for (var i = 0; i < inputs.length; i++) {
-    inputs[i].addEventListener('keyup', function(){
-       if(this.value.length>=1) {
-        this.nextElementSibling.classList.add('fijar');
-       } else {
-        this.nextElementSibling.classList.remove('fijar');
-       }
-    });
-}
+document.addEventListener("DOMContentLoaded", function() {
+const form = document.querySelector('#contacto');
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    console.log('Formulario enviado');
+    const nombre = document.querySelector('#contacto input[name="nombre"]').value;
+    console.log(`Nombre: ${nombre}`);
+    const correo = document.querySelector('#contacto input[name="correo"]').value;
+    console.log(`Correo: ${correo}`);
+    const telefono = document.querySelector('#contacto input[name="telefono"]').value;
+    console.log(`Teléfono: ${telefono}`);
+    const mensaje = document.querySelector('#contacto textarea[name="mensaje"]').value;
+    console.log(`Mensaje: ${mensaje}`);
+    const pdf = new jsPDF();
+    pdf.text(`Nombre: ${nombre}\nCorreo: ${correo}\nTeléfono: ${telefono}\nMensaje: ${mensaje}`, 10, 10);
+    pdf.save("contacto.pdf");
+  });
 
-function toggleMenu() {
-    var menu = document.querySelector('.menu');
-    menu.classList.toggle('show');
-  }
+});
